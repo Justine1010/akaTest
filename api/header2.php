9949adf42718f1f5b4808e956db05d1126be11ad
Justine1010 <justinehui1010@gmail.com>
2023-11-12 23:57:12 +0800

    add all

---
+++ api/header2.php
@@ -0,0 +1,127 @@
+<?php
+    $baseHref = "https://caebusinesssolutions.co.uk/aka";
+    
+?>
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Corporate communication for Brussels technology companies | Nove</title>
+    <meta name='robots' content='max-image-preview:large' />
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Public affairs and communication">
+    <meta property="og:description"
+        content="NOVE is an advisory agency specialized in public affairs and communication based in Brussels.">
+    <meta property="og:site_name" content="Nove">
+    <meta property="og:locale" content="en_US">
+    <meta name="description"
+        content="NOVE is an advisory agency specialized in public affairs and communication based in Brussels." />
+    <meta name="theme-color" content="#13294b">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+    <link rel='stylesheet' id='google-fonts-1-css'
+        href='https://fonts.googleapis.com/css?family=Roboto%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic%7CRoboto+Slab%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic&#038;display=swap&#038;ver=6.1.1'
+        media='all' />
+    <link rel='stylesheet' href='<?php echo $baseHref; ?>/css/globalStyles2.css' />
+    <link rel="shortcut icon" href="" type="image/x-icon">
+    <link rel='stylesheet' href='<?php echo $baseHref; ?>/css/style2.css?v=1.2' type='text/css' media='all' />
+    <link rel='stylesheet' href='<?php echo $baseHref; ?>/css/style-louis2.css' type='text/css' media='all' />
+    <script src="<?php echo $baseHref; ?>/scripts/jquery-3.2.1.min.js"></script>
+    <script src="<?php echo $baseHref; ?>/scripts/jquery-migrate.min.js"></script>
+    <link rel="icon" type="image/x-icon" href="<?php echo $baseHref; ?>/assets/faviconBlack.png">
+    <style>
+	#barba-wrapper::before{
+	content: "";
+	position:absolute;
+	background:transparent;
+	top:0px;
+	left:0px;
+	width:100%;
+	height:100%;
+	z-index:99;
+	pointer-events: none;
+}
+.home #barba-wrapper::before{
+	content: "";
+	position:absolute;
+	background:rgb(0 0 0 / 45%);
+	top:0px;
+	left:0px;
+	width:100%;
+	height:100%;
+	z-index:99;
+	pointer-events: all;
+}
+#logo3{
+	width: 150px;
+	overflow: hidden;
+}
+#logo3 img{
+	width: 105%;
+	margin-left: -2px;
+	margin-top: -2px;
+	margin-bottom: -7px;
+}
+#logo1{
+	width: 400px;
+	overflow: hidden;
+}
+#logo1 img{
+	width: 105%;
+	margin-left: -2px;
+	margin-top: -2px;
+	margin-bottom: -7px;
+}
+nav #contact>div p span {
+	padding-right:4px !important;
+}
+.textJust {
+	hyphens: none !important;
+	
+}
+.menuHome a:nth-child(2):after{
+	background-color:rgba(0,0,0,0.6);
+}
+.menuHome a:nth-child(3):after{
+	background-color:rgba(255,255,255,0.8);
+}
+.menuHome a:nth-child(3):hover > div > p{
+	color:#000 !important
+}
+@media(max-width:959px){
+	#colDroite1.menuVertical, #colDroite2.menuVertical {
+		background: #ffff;
+	}
+	#colDroite1.menuDr.menuVertical a{
+		color:#000 !important;
+	}
+	#colDroite1.menuVertical h2{
+		color:#000 !important;
+	}
+	#colDroite1.menuVertical h2::after, #colDroite2.menuVertical h2::after {  
+	  border-bottom: #000 1px solid;
+	  border-right: #000 1px solid;  
+	}
+	#colDroite1.menuVertical h2::before, #colDroite2.menuVertical h2::before {  
+	  border-top: #000 1px solid;
+	  border-left: #000 1px solid;
+	}
+	#colDroite2.menuVertical a{
+		color:#000 !important;
+	}
+	#colDroite2.menuVertical h2{
+		padding:18px !important;
+		color:#000;
+	}
+	body #colDroite2.menuVertical a {
+		padding: 00px !important;
+	}
+	#colDroite1 .titreBloc.miniTitre span, #colDroite2 .titreBloc.miniTitre span {
+	  padding-left: 0 !important;
+	  font-family: akkuratbold !important;
+	}
+}
+    </style>
+</head>
